41a1df7976b067fafda3914020371e97d1e92b85
shakiba-shademani <shakiba.sh75@gmail.com>
2024-01-14 17:34:59 +0330

    checkbox tree was added

--- src/app/(hydrogen)/react-tree-sample/page.tsx
+++ src/app/(hydrogen)/react-tree-sample/page.tsx
@@ -1,12 +1,13 @@
 'use client';
-
 import { Datepicker } from '@/components/ui/datepicker';
 import SortableTree from '@nosferatu500/react-sortable-tree';
 import '@nosferatu500/react-sortable-tree/style.css';
 import { data } from './data.json';
 import { useState } from 'react';
+import { Button } from 'rizzui';
+import WidgetCard from '@/components/cards/widget-card';
 
-function Shakiba() {
+function List() {
   const finalData = data.map((item) => {
     const expandedItem = { ...item, expanded: true };
     return expandedItem;
@@ -26,29 +27,31 @@ function Shakiba() {
           // disabled
         />
       </div>
-      <div className="h-[600px] bg-slate-100">
-        <SortableTree
-          rowDirection="rtl"
-          maxDepth={1}
-          canDrag={() => false}
-          treeData={treeData}
-          onChange={(data) => setTreeData(data)}
-          generateNodeProps={({ node }) => ({
-            buttons: [
-              <button
-                type="button"
-                key={node.id}
-                className="bg-blue-400"
-                onClick={() => console.log(node)}
-              >
-                جزییات
-              </button>,
-            ],
-          })}
-        />
-      </div>
+      <WidgetCard title="منو">
+        <div className="h-[600px]">
+          <SortableTree
+            rowDirection="rtl"
+            maxDepth={1}
+            canDrag={() => false}
+            treeData={treeData}
+            onChange={(data) => setTreeData(data)}
+            generateNodeProps={({ node }) => ({
+              buttons: [
+                <Button
+                  type="button"
+                  key={node.id}
+                  className="h-8 w-16"
+                  onClick={() => console.log(node)}
+                >
+                  جزییات
+                </Button>,
+              ],
+            })}
+          />
+        </div>
+      </WidgetCard>
     </>
   );
 }
 
-export default Shakiba;
+export default List;

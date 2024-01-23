d408870df0e4cc7e6644434d8a9b84ec1a241ad8
binahub <binatube@gmail.com>
2024-01-23 09:40:17 +0330

    other pages changes

--- src/app/(other-pages)/not-found/page.tsx
+++ src/app/(other-pages)/not-found/page.tsx
@@ -12,33 +12,30 @@ import NotFoundImg from '@public/not-found.png';
 export default function NotFoundPage() {
   const { push } = useRouter();
   return (
-    <div className="flex grow items-center px-6 xl:px-10">
-      <div className="mx-auto text-center">
+    <div className='flex grow items-center px-6 xl:px-10'>
+      <div className='mx-auto text-center'>
         <Image
           src={NotFoundImg}
-          alt="not found"
-          className="mx-auto mb-8 aspect-[360/326] max-w-[256px] xs:max-w-[370px] lg:mb-12 2xl:mb-16"
+          alt='not found'
+          className='mx-auto mb-8 aspect-[360/326] max-w-[256px] xs:max-w-[370px] lg:mb-12 2xl:mb-16'
         />
-        <Title
-          as="h1"
-          className="text-[22px] font-bold leading-normal text-gray-1000 lg:text-3xl"
-        >
-          Sorry, the page not found
+        <Title as='h1' className='text-[22px] font-bold leading-normal text-gray-1000 lg:text-3xl'>
+          صفحه مورد نظر یافت نشد!
         </Title>
-        <Text className="mt-3 text-sm leading-loose text-gray-500 lg:mt-6 lg:text-base lg:leading-loose">
+        {/* <Text className="mt-3 text-sm leading-loose text-gray-500 lg:mt-6 lg:text-base lg:leading-loose">
           We have been spending long hours in order to launch our new website.
           Join our
           <br className="hidden sm:inline-block" />
           mailing list or follow us on Facebook for get latest update.
-        </Text>
+        </Text> */}
         <Button
-          color="primary"
-          size="xl"
-          className="mt-8 h-12 px-4 xl:h-14 xl:px-6"
+          color='primary'
+          size='xl'
+          className='mt-8 h-12 px-4 xl:h-14 xl:px-6'
           onClick={() => push('/')}
         >
-          <PiHouseLineBold className="mr-1.5 text-lg" />
-          Back to home
+          <PiHouseLineBold className='ml-1.5 text-lg' />
+          بازگشت
         </Button>
       </div>
     </div>

ab60379331dcd4ceb243f5cafa91af545623b98f
Shakiba <z.shadmani@sadadpsp.ir>
2024-01-28 14:54:03 +0330

    rowsNumber was added to table skeleton

--- src/components/skeleton/table.tsx
+++ src/components/skeleton/table.tsx
@@ -2,42 +2,43 @@ import { LineGroup, Skeleton } from '@/components/ui/skeleton';
 
 export interface TableSkeletonProps {
   actionButton?: boolean;
+  rowsNumber?: number;
 }
 
-export const TableSkeleton = ({ actionButton }: TableSkeletonProps) => {
+export const TableSkeleton = ({ actionButton, rowsNumber }: TableSkeletonProps) => {
   const TableRowSkeleton = () =>
     actionButton ? (
-      <div className='flex justify-between items-center p-4 my-2 h-[2rem]'>
-        <LineGroup columns={3} className='grid-cols-3 gap-8 w-[85%]' skeletonClassName='h-8' />
-        <LineGroup
-          skeletonClassName='h-8 w-8'
-          columns={3}
-          className='grid-cols-3 gap-0.5 w-[10%]'
-        />
-      </div>
+      <>
+        <div className='flex justify-between items-center p-4 my-2 h-[2rem]'>
+          <LineGroup columns={3} className='grid-cols-3 gap-8 w-[85%]' skeletonClassName='h-8' />
+          <LineGroup
+            skeletonClassName='h-8 w-8'
+            columns={3}
+            className='grid-cols-3 gap-0.5 w-[10%]'
+          />
+        </div>
+        <hr />
+      </>
     ) : (
-      <div className='flex justify-between items-center p-4 my-2 h-[2rem]'>
-        <LineGroup columns={4} className='grid-cols-4 gap-8 w-full' skeletonClassName='h-8' />
-      </div>
+      <>
+        <div className='flex justify-between items-center p-4 my-2 h-[2rem]'>
+          <LineGroup columns={4} className='grid-cols-4 gap-8 w-full' skeletonClassName='h-8' />
+        </div>
+        <hr />
+      </>
     );
+
+  let countArray: JSX.Element[] = [];
+
+  for (let i = 1; i <= (rowsNumber || 7); i++) {
+    countArray.push(<TableRowSkeleton key={i} />);
+  }
+
   return (
     <>
       <Skeleton className='h-10' />
       <br />
-      <TableRowSkeleton />
-      <hr />
-      <TableRowSkeleton />
-      <hr />
-      <TableRowSkeleton />
-      <hr />
-      <TableRowSkeleton />
-      <hr />
-      <TableRowSkeleton />
-      <hr />
-      <TableRowSkeleton />
-      <hr />
-      <TableRowSkeleton />
-      <hr />
+      {countArray}
     </>
   );
 };

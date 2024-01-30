0e7a8af381f128551813f8b2bba0df27421235ce
Shakiba <z.shadmani@sadadpsp.ir>
2024-01-30 11:22:06 +0330

    fixing sweet alert

--- src/app/(hydrogen)/sweet-alert-sample/page.tsx
+++ src/app/(hydrogen)/sweet-alert-sample/page.tsx
@@ -1,24 +1,43 @@
 'use client';
 import WidgetCard from '@/components/cards/widget-card';
 import SweetAlert from '@/components/ui/sweet-alert';
+import Notification from '@/utils/notification';
 import { Button } from 'rizzui';
 
 function page() {
   return (
     <div>
-      <WidgetCard title='component using'>
+      <WidgetCard title='using sweet alert' className='flex flex-col'>
         <Button
-          onClick={
-            () =>
-              SweetAlert({
-                type: 'error',
-                message: 'خطایی رخ داده است.',
-                cancelButtonText: 'ای بابا',
-              })
-            // Notification.success("با موفقیت انجانم شد.")
+          className='my-4 w-40'
+          onClick={() =>
+            SweetAlert({
+              type: 'info',
+              message: 'تغییرات انجام شد.',
+              confirmFlow: () => console.log('انجام شد.'),
+            })
           }
         >
-          click to see sweet alert
+          info sweet alert
+        </Button>
+        <Button className='mb-4 w-40' onClick={() => Notification.success('با موفقیت انجام شد.')}>
+          success sweet alert
+        </Button>
+        <Button className='mb-4 w-40' onClick={() => Notification.error('خطایی رخ داده است.')}>
+          error sweet alert
+        </Button>
+        <Button
+          className='mb-4 w-40'
+          onClick={() =>
+            SweetAlert({
+              type: 'warning',
+              message: 'موارد الزامی را تکمیل کنید.',
+              cancelButtonText: 'ای بابا',
+              dismissFlow: () => console.log('لغو شد.'),
+            })
+          }
+        >
+          warning sweet alert
         </Button>
       </WidgetCard>
     </div>
--- src/components/ui/sweet-alert.tsx
+++ src/components/ui/sweet-alert.tsx
@@ -5,11 +5,13 @@ import withReactContent from 'sweetalert2-react-content';
 const MySwal = withReactContent(Swal);
 
 interface AlertProps {
-  type: 'error' | 'success' | 'warning';
+  type: 'error' | 'success' | 'warning' | 'info';
   title?: string;
   message: string;
   confirmButtonText?: string;
   cancelButtonText?: string;
+  confirmFlow?: () => void;
+  dismissFlow?: () => void;
 }
 
 export default function SweetAlert({
@@ -18,16 +20,26 @@ export default function SweetAlert({
   message,
   confirmButtonText,
   cancelButtonText,
+  confirmFlow,
+  dismissFlow,
 }: AlertProps) {
   let ButtonClass = '';
+  let color = '';
   if (type === 'error') ButtonClass = 'bg-[#ea5455]';
   else if (type === 'success') ButtonClass = 'bg-[#5cb85c]';
-  else ButtonClass = 'bg-[#facea8]';
+  else if (type === 'info') {
+    ButtonClass = 'bg-primary';
+    color = 'rgb(56 114 250)';
+  } else {
+    ButtonClass = 'bg-[#FBBA00]';
+    color = '#FBBA00';
+  }
 
   const handleSweetAlert = () =>
     MySwal.fire({
       title,
       icon: type,
+      iconColor: color,
       text: message,
       showCancelButton: !!cancelButtonText,
       confirmButtonText: confirmButtonText || 'باشه!',
@@ -35,9 +47,15 @@ export default function SweetAlert({
       focusConfirm: false,
       allowOutsideClick: false,
       customClass: {
-        confirmButton: `${ButtonClass} p-2 rounded-lg`,
+        confirmButton: `${ButtonClass} p-2 rounded-lg text-white`,
       },
       buttonsStyling: false,
+    }).then((result) => {
+      if (confirmFlow && result.isConfirmed) {
+        confirmFlow();
+      } else if (dismissFlow && result.isDismissed) {
+        dismissFlow();
+      }
     });
 
   return handleSweetAlert();

54e0e9f5afadd16c83b6a9dbb1a9ddd29b0911b4
binahub <binatube@gmail.com>
2024-02-14 08:32:09 +0330

    comment for test husky!

--- src/app/(hydrogen)/food/list/page.tsx
+++ src/app/(hydrogen)/food/list/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useTable } from '@/hooks/use-table';
 import { routes } from '@/config/routes';
+//@TODO: should change import from package
 import Table from '@/app/shared/table/table';
 import FilterElement from '@/app/shared/table/content-filter';
 import { detail } from './detail';

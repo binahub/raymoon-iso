49bc170e002c1f881713c876047fad47bdd73f45
binahub <binatube@gmail.com>
2024-02-18 17:07:49 +0330

    add content-filter before change

--- src/app/shared/table/content-filter.tsx
+++ src/app/shared/table/content-filter.tsx
@@ -1,9 +1,10 @@
-"use client";
-import { useEffect, useState } from "react";
-import { StatusField } from "./status-field";
-import { Input } from "@/components/ui/input";
-import { Button } from "rizzui";
-import { PiTrashDuotone } from "react-icons/pi";
+'use client';
+//page mored nazar
+import { useEffect, useState } from 'react';
+import { StatusField } from './status-field';
+import { Input } from '@/components/ui/input';
+import { Button } from 'rizzui';
+import { PiTrashDuotone } from 'react-icons/pi';
 
 type FilterElementProps = {
   isFiltered?: boolean;
@@ -64,7 +65,7 @@ export default function FilterElement({
   const onClickFilter = (e: any) => {
     e.preventDefault();
     localFilter?.forEach((element: any) => {
-      if (element.value === "") return;
+      if (element.value === '') return;
       if (actionFilter) {
         actionFilter({ ...filters, [element.key]: element.value });
       } else updateFilter(element.key, element.value);
@@ -76,19 +77,19 @@ export default function FilterElement({
    */
   function getElementForm(item: any) {
     switch (item.type) {
-      case "Text":
+      case 'Text':
         return (
           <Input
-            type="text"
+            type='text'
             label={item.label}
-            color="info"
-            value={localFilter.find((f) => f.key === item.key)?.value || ""}
+            color='info'
+            value={localFilter.find((f) => f.key === item.key)?.value || ''}
             onChange={(event) => OnChangeInput(event, item)}
             autoFocus
-            className="bg-white"
+            className='bg-white'
           />
         );
-      case "Select":
+      case 'Select':
         return (
           <StatusField
             options={item.selectOption}
@@ -101,7 +102,7 @@ export default function FilterElement({
             label={item.label}
           />
         );
-      case "DatePicker":
+      case 'DatePicker':
         return;
       default:
         undefined;
@@ -110,29 +111,29 @@ export default function FilterElement({
 
   return (
     dataFilter && (
-      <div className="relative ">
+      <div className='relative '>
         {dataFilter?.map((item: any, index: number) => (
-          <div className="mt-5" key={index}>
+          <div className='mt-5' key={index}>
             {getElementForm(item)}
           </div>
         ))}
         {localFilter?.some((f) => f.value) ? (
           <Button
-            size="sm"
+            size='sm'
             onClick={() => {
               setLocalFilter([]);
               handleReset();
             }}
-            className="absolute inset-x-0 bottom-20 h-11 w-[100%]  bg-gray-200/70 "
-            variant="flat"
+            className='absolute inset-x-0 bottom-20 h-11 w-[100%]  bg-gray-200/70 '
+            variant='flat'
           >
-            <PiTrashDuotone className="me-1.5 h-[17px] w-[17px]" /> پاک کن
+            <PiTrashDuotone className='me-1.5 h-[17px] w-[17px]' /> پاک کن
           </Button>
         ) : null}
         <Button
-          size="lg"
+          size='lg'
           onClick={(event) => onClickFilter(event)}
-          className="absolute inset-x-0 bottom-0 bg-blue-darkBlue text-sm"
+          className='absolute inset-x-0 bottom-0 bg-blue-darkBlue text-sm'
         >
           اعمال فیلتر
         </Button>

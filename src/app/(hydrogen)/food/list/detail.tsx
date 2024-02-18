18d12ad2c75895129d914e00183560823093facb
binahub <binatube@gmail.com>
2024-02-18 18:41:23 +0330

    remove edit and add detail

--- src/app/(hydrogen)/food/list/detail.tsx
+++ src/app/(hydrogen)/food/list/detail.tsx
@@ -1,3 +1,5 @@
+/* show more details in expect table (readOnly) */
+
 'use client ';
 import { Input } from '@/components/ui/input';
 
@@ -10,22 +12,22 @@ type Props = {
 
 export function detail(rowData: Props) {
   return (
-    <div className="my-10 flex justify-center">
-      <div className=" grid w-[70%] grid-cols-2 gap-5 rounded-2xl border border-gray-100 bg-white dark:bg-gray-100 p-12">
+    <div className='my-10 flex justify-center'>
+      <div className=' grid w-[70%] grid-cols-2 gap-5 rounded-2xl border border-gray-100 bg-white dark:bg-gray-100 p-12'>
         <div>
           <Input
-            label="شماره مرجع"
-            labelClassName="font-medium text-gray-900 dark:text-white"
-            className="mb-5"
+            label='نام'
+            labelClassName='font-medium text-gray-900 dark:text-white'
+            className='mb-5'
             defaultValue={rowData?.record?.name}
             disabled
           />
         </div>
         <div>
           <Input
-            label=" شماره پیگیری"
-            labelClassName="font-medium text-gray-900 dark:text-white"
-            className="mb-5"
+            label=' توضیحات'
+            labelClassName='font-medium text-gray-900 dark:text-white'
+            className='mb-5'
             defaultValue={rowData?.record?.description}
             disabled
           />
@@ -34,4 +36,3 @@ export function detail(rowData: Props) {
     </div>
   );
 }
-

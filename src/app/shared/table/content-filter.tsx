ac5c0873f69f12652b97fbd3ea7cd5e57ab5ef36
binahub <binatube@gmail.com>
2024-02-18 19:01:09 +0330

    content filter change

--- src/app/shared/table/content-filter.tsx
+++ src/app/shared/table/content-filter.tsx
@@ -1,8 +1,8 @@
 'use client';
-//page mored nazar
 import { useEffect, useState } from 'react';
 import { StatusField } from './status-field';
 import { Input } from '@/components/ui/input';
+import { Datepicker } from '@/components/ui/datepicker';
 import { Button } from 'rizzui';
 import { PiTrashDuotone } from 'react-icons/pi';
 
@@ -25,7 +25,9 @@ export default function FilterElement({
   actionFilter,
   setIsOpenDrawer,
 }: FilterElementProps) {
-  const [localFilter, setLocalFilter] = useState<{ key: string; value: string }[]>([]);
+  const [localFilter, setLocalFilter] = useState<{ key: string; value: string }[]>(
+    filters ? Object.keys(filters).map((key) => ({ key, value: filters[key] })) : []
+  );
 
   useEffect(() => {
     setLocalFilter(Object.keys(filters).map((key) => ({ key, value: filters[key] })));
@@ -60,16 +62,36 @@ export default function FilterElement({
   };
 
   /*
+   *hendle datePicker onchange
+   */
+  const OnChangeDatePicker = (value: any, item: any) => {
+    let data = [...localFilter];
+    const finded = data.find((f) => f.key === item.key);
+    if (finded) {
+      finded.value = value;
+    } else {
+      data.push({ key: item.key, value: value });
+    }
+    setLocalFilter(data);
+  };
+
+  /*
    *hendle filter action
    */
   const onClickFilter = (e: any) => {
     e.preventDefault();
     localFilter?.forEach((element: any) => {
-      if (element.value === '') return;
-      if (actionFilter) {
-        actionFilter({ ...filters, [element.key]: element.value });
-      } else updateFilter(element.key, element.value);
+      updateFilter(element.key, element.value);
     });
+
+    let tempData: any = {};
+    localFilter?.forEach((element: any) => {
+      tempData[element.key] = element.value;
+    });
+
+    if (actionFilter) {
+      actionFilter({ ...filters, ...tempData });
+    }
   };
 
   /*
@@ -94,8 +116,8 @@ export default function FilterElement({
           <StatusField
             options={item.selectOption}
             value={localFilter.find((f) => f.key === item.key)?.value}
-            onChange={(event) => OnChangeSelect(event, item)}
-            getOptionValue={(option) => option.name}
+            onChange={(event: any) => OnChangeSelect(event, item)}
+            getOptionValue={(option: any) => option.name}
             displayValue={(selected: string) =>
               item?.selectOption.find((option: any) => option.value === selected)?.label ?? selected
             }
@@ -103,7 +125,14 @@ export default function FilterElement({
           />
         );
       case 'DatePicker':
-        return;
+        return (
+          <Datepicker
+            label={item.label}
+            value={localFilter.find((f) => f.key === item.key)?.value}
+            onChange={(event: any) => OnChangeDatePicker(event, item)}
+            // dateFormat='yyyy/MM/dd'
+          />
+        );
       default:
         undefined;
     }
@@ -111,32 +140,37 @@ export default function FilterElement({
 
   return (
     dataFilter && (
-      <div className='relative '>
-        {dataFilter?.map((item: any, index: number) => (
-          <div className='mt-5' key={index}>
-            {getElementForm(item)}
-          </div>
-        ))}
-        {localFilter?.some((f) => f.value) ? (
+      <div className='h-screen'>
+        <div className='h-[70%]  overflow-y-auto pl-3 '>
+          {dataFilter?.map((item: any, index: number) => (
+            <div className='mt-5' key={index}>
+              {getElementForm(item)}
+            </div>
+          ))}
+        </div>
+
+        <div className='h-[15%] mt-4 flex flex-col justify-end items-end'>
+          {localFilter?.some((f) => f.value) ? (
+            <Button
+              size='sm'
+              onClick={() => {
+                setLocalFilter([]);
+                handleReset();
+              }}
+              className=' h-11 w-[100%]  bg-gray-200/70 mb-6 '
+              variant='flat'
+            >
+              <PiTrashDuotone className='me-1.5 h-[17px] w-[17px]' /> پاک کن
+            </Button>
+          ) : null}
           <Button
-            size='sm'
-            onClick={() => {
-              setLocalFilter([]);
-              handleReset();
-            }}
-            className='absolute inset-x-0 bottom-20 h-11 w-[100%]  bg-gray-200/70 '
-            variant='flat'
+            size='lg'
+            onClick={(event) => onClickFilter(event)}
+            className=' bg-blue-darkBlue text-sm  w-[100%] '
           >
-            <PiTrashDuotone className='me-1.5 h-[17px] w-[17px]' /> پاک کن
+            اعمال فیلتر
           </Button>
-        ) : null}
-        <Button
-          size='lg'
-          onClick={(event) => onClickFilter(event)}
-          className='absolute inset-x-0 bottom-0 bg-blue-darkBlue text-sm'
-        >
-          اعمال فیلتر
-        </Button>
+        </div>
       </div>
     )
   );

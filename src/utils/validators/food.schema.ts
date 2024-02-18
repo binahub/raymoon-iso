cd69dc75363944bf7179f5653fe9e5a780cc8cab
binahub <binatube@gmail.com>
2024-02-18 16:51:57 +0330

    add food.schema

---
+++ src/utils/validators/food.schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+// form zod validation schema
+
+export const foodSchema = z.object({
+  id: z.string(),
+  name: z.string().min(1, { message: 'ضروری' }),
+  description: z.string().min(1, { message: 'ضروری' }),
+});
+
+// generate form types from zod validation schema
+export type FoodSchema = z.infer<typeof foodSchema>;

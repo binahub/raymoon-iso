fa803ea9cee315b159b3905f3defe8162ec1077d
binahub <binatube@gmail.com>
2024-02-18 17:15:58 +0330

    add table-pagination

--- src/app/shared/table/table-pagination.tsx
+++ src/app/shared/table/table-pagination.tsx
@@ -22,49 +22,51 @@ export function TablePagination({
   paginatorClassName = 'mt-5 xs:mt-6 sm:mt-7',
   ...props
 }: TablePaginationProps) {
-  // if (total && total < pageSize) {
-  //   return null;
-  // }
-
   return (
-    <div
-      className={cn(
-        'table-pagination flex items-center justify-center sm:justify-between',
-        paginatorClassName
-      )}
-    >
-      {!setPageSize ? (
-        total && (
-          <div className='hidden text-gray-500 sm:inline-flex'>
-            {props.current} of {Math.ceil(total / pageSize)} pages
-          </div>
-        )
-      ) : (
-        <div className='hidden items-center sm:flex'>
-          {/* تعداد رکورد در هر صفحه:{' '} */}
-          <Select
-            options={paginationLimitOptions}
-            onChange={setPageSize}
-            size='sm'
-            variant='flat'
-            value={pageSize}
-            getOptionValue={({ value }) => value}
-            suffix={<PiCaretDownBold />}
-            useContainerWidth={false}
-            dropdownClassName='p-1 border w-12 border-gray-100 shadow-lg'
-            className='ms-1 [&_button]:font-medium'
+    <>
+      {total && total !== 0 && total > 5 ? (
+        <div
+          className={cn(
+            'table-pagination flex items-center justify-center sm:justify-between',
+            paginatorClassName
+          )}
+        >
+          {!setPageSize ? (
+            total && (
+              <div className='hidden text-gray-500 sm:inline-flex'>
+                {props.current} of {Math.ceil(total / pageSize)} صفحه
+              </div>
+            )
+          ) : (
+            <div className='hidden items-center sm:flex'>
+              {/* تعداد رکورد در هر صفحه:{' '} */}
+              <Select
+                options={paginationLimitOptions}
+                onChange={setPageSize}
+                size='sm'
+                variant='flat'
+                value={pageSize}
+                getOptionValue={({ value }) => value}
+                suffix={<PiCaretDownBold />}
+                useContainerWidth={false}
+                dropdownClassName='p-1 border w-12 border-gray-100 shadow-lg'
+                className='ms-1 [&_button]:font-medium'
+              />
+            </div>
+          )}
+          <Pagination
+            total={total}
+            pageSize={pageSize}
+            defaultCurrent={1}
+            showLessItems={true}
+            prevIconClassName='py-0 text-gray-500 !leading-[26px]'
+            nextIconClassName='py-0 text-gray-500 !leading-[26px]'
+            {...props}
           />
         </div>
+      ) : (
+        ''
       )}
-      <Pagination
-        total={total}
-        pageSize={pageSize}
-        defaultCurrent={1}
-        showLessItems={true}
-        prevIconClassName='py-0 text-gray-500 !leading-[26px]'
-        nextIconClassName='py-0 text-gray-500 !leading-[26px]'
-        {...props}
-      />
-    </div>
+    </>
   );
 }

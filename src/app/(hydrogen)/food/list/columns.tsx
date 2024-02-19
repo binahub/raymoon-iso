3f3dd248b90beac9549b009e56cec07ca9583125
unknown <fmirzaie9279@gmail.com>
2024-02-19 15:21:18 +0330

    add json-server

--- src/app/(hydrogen)/food/list/columns.tsx
+++ src/app/(hydrogen)/food/list/columns.tsx
@@ -80,7 +80,7 @@ function RenderAction({
   return (
     <>
       <div className='flex items-center justify-end gap-3 pe-4'>
-        <Tooltip size='sm' content={() => ' ویرایش با مدال'} placement='top' color='invert'>
+        {/* <Tooltip size='sm' content={() => ' ویرایش با مدال'} placement='top' color='invert'>
           <ActionIcon
             tag='span'
             size='sm'
@@ -96,7 +96,7 @@ function RenderAction({
           >
             <PencilIcon className='h-4 w-4' />
           </ActionIcon>
-        </Tooltip>
+        </Tooltip> */}
         <Tooltip
           size='sm'
           content={() => ' ویرایش جزئیات در صفحه جدید'}

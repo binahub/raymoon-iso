0835872e0709c906fb0963cec1074678a7ad741c
Shakiba <z.shadmani@sadadpsp.ir>
2024-02-20 16:34:47 +0330

    textarea component was changed

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,3 +1,13 @@
 'use client';
+import { forwardRef } from 'react';
+import { Textarea as RizzTextArea, type TextareaProps } from 'rizzui';
 
-export { Textarea } from 'rizzui';
+export { type TextareaProps } from 'rizzui';
+
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ color, ...props }, ref) => {
+    return <RizzTextArea color={color || 'info'} {...props} ref={ref} />;
+  }
+);
+
+Textarea.displayName = 'Textarea';

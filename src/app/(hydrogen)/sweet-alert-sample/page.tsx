1be5e2d4b2f26c75b9abd53c174844c4c0b775ca
binahub <binatube@gmail.com>
2024-02-24 14:19:26 +0330

    card add V65

--- src/app/(hydrogen)/sweet-alert-sample/page.tsx
+++ src/app/(hydrogen)/sweet-alert-sample/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 // import WidgetCard from '@/components/cards/widget-card';
 import SweetAlert from '@/components/ui/sweet-alert';
-import { Button, Datepicker, Input, Spinner, WidgetCard } from 'shafa-bo';
+import { Button, Datepicker, Input, Spinner, WidgetCard, Card } from 'shafa-bo';
 
 function page() {
   return (
@@ -18,10 +18,13 @@ function page() {
         >
           click to see sweet alert
         </Button>
-        <Datepicker />
+        <Datepicker onChange={() => console.log('date changed')} />
         <Input />
         <Spinner size='sm' />
       </WidgetCard>
+      <Card height='[150px]' className='p-5'>
+        salam
+      </Card>
     </div>
   );
 }

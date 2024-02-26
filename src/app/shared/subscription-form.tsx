debd6613891c27497aa83bb0339a5ee588b1f1fe
binahub <binatube@gmail.com>
2024-02-26 11:42:48 +0330

    xxx

--- src/app/shared/subscription-form.tsx
+++ src/app/shared/subscription-form.tsx
@@ -2,10 +2,7 @@
 
 import z from 'zod';
 import { SubmitHandler } from 'react-hook-form';
-import { Form } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-
+import { Form, Input, Button } from 'shafa-bo';
 // import images and icons
 import { PiBellBold } from 'react-icons/pi';
 
@@ -24,25 +21,20 @@ export default function SubscriptionForm() {
   return (
     <Form validationSchema={subscriptionFormSchema} onSubmit={onSubmit}>
       {({ register, formState: { errors } }) => (
-        <div className="mx-auto flex max-w-lg flex-col gap-3 lg:ml-0 lg:max-w-full lg:flex-row xl:gap-5">
+        <div className='mx-auto flex max-w-lg flex-col gap-3 lg:ml-0 lg:max-w-full lg:flex-row xl:gap-5'>
           <Input
-            size="xl"
-            type="email"
-            color="primary"
-            variant="outline"
-            className="grow pb-1 lg:max-w-[384px]"
-            placeholder="example@google.com"
-            inputClassName="h-12 xl:h-14 [&>input]:text-center lg:[&>input]:text-start"
+            size='xl'
+            type='email'
+            color='primary'
+            variant='outline'
+            className='grow pb-1 lg:max-w-[384px]'
+            placeholder='example@google.com'
+            inputClassName='h-12 xl:h-14 [&>input]:text-center lg:[&>input]:text-start'
             {...register('email')}
             error={errors.email?.message}
           />
-          <Button
-            size="xl"
-            type="submit"
-            color="primary"
-            className="h-12 px-4 xl:h-14 xl:px-6"
-          >
-            <PiBellBold className="me-1.5 text-lg" /> Notify Me
+          <Button size='xl' type='submit' color='primary' className='h-12 px-4 xl:h-14 xl:px-6'>
+            <PiBellBold className='me-1.5 text-lg' /> Notify Me
           </Button>
         </div>
       )}

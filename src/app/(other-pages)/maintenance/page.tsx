dc5183ffa7a6e18f5f6c1b6e2277a6e315182c1c
binahub <binatube@gmail.com>
2024-02-26 11:49:36 +0330

    ppp

--- src/app/(other-pages)/maintenance/page.tsx
+++ src/app/(other-pages)/maintenance/page.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image';
-// import { Title, Text, Input } from 'shafa-bo';
-import SubscriptionForm from '@/app/shared/subscription-form';
-import MaintananceImg from '@public/maintanance.png';
+// import Image from 'next/image';
+import { Title, Text, Input } from 'shafa-bo';
+// import SubscriptionForm from '@/app/shared/subscription-form';
+// import MaintananceImg from '@public/maintanance.png';
 
 export default function MaintenancePage() {
   return (
     <div className='flex grow items-center px-6 xl:px-10'>
       <div className='mx-auto flex w-full max-w-[1520px] flex-col-reverse items-center justify-between gap-5 text-center lg:flex-row lg:text-start'>
         <div>
-          {/* <Input /> */}
+          <Input />
           {/* <Title
             as='h1'
             className='mb-3 text-[22px] font-bold leading-snug text-gray-1000 sm:text-2xl md:mb-5 md:text-3xl md:leading-snug xl:mb-7 xl:text-4xl xl:leading-normal 2xl:text-[40px] 3xl:text-5xl 3xl:leading-snug'
@@ -24,15 +24,15 @@ export default function MaintenancePage() {
           <Text className='mb-4 mt-8 text-sm font-semibold leading-normal text-gray-800 md:mt-10 xl:mb-6 xl:mt-12 xl:text-base'>
             Don’t want to miss update? Subscribe now
           </Text> */}
-          <SubscriptionForm />
+          {/* <SubscriptionForm /> */}
         </div>
-        <div className='pt-5 lg:pt-0'>
+        {/* <div className='pt-5 lg:pt-0'>
           <Image
             src={MaintananceImg}
             alt='maintanance'
             className='aspect-[768/558] max-w-[320px] dark:invert sm:max-w-sm xl:max-w-[580px] 2xl:max-w-[768px]'
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );

f87fdab74015673aac60a7f7e1c1ddcebc059388
unknown <fmirzaie9279@gmail.com>
2024-03-06 13:34:26 +0330

    fix refactor

--- src/app/(hydrogen)/feature-sample/list/filter.tsx
+++ src/app/(hydrogen)/feature-sample/list/filter.tsx
@@ -2,7 +2,6 @@ import { type GeneratedFilterType } from 'shafa-bo';
 /* initioalization  */
 export const initialFilterValues : any = {
   name: '',
-  // description: '',
 };
 
 /* filterable form input */
